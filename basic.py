a7e7b54de94122f4a8486a5194e6425fe0a9d2bd
Mav <mespin11@jh.edu>
2024-05-21 19:43:37 -0700

    added gradient + second page + edited requirements.txt

--- basic.py
+++ basic.py
@@ -1,7 +1,9 @@
 from neoscore.core.neoscore import setup, show
 from neoscore.core.text import Text
 from neoscore.core.point import ORIGIN
+from neoscore.common import *
 
 setup()
 Text(ORIGIN, None, "Hello, neoscore!")
+second_page = neoscore.document.pages[1]
 show()
